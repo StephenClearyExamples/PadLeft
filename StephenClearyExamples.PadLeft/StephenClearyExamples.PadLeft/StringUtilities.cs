fdf19ebe5b2ba19f3b37f9777925b52e255536da
Stephen Cleary <StephenCleary@users.noreply.github.com>
2017-05-10 17:35:45 -0400

    v1.0.0

---
+++ StephenClearyExamples.PadLeft/StephenClearyExamples.PadLeft/StringUtilities.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StephenClearyExamples.PadLeft
+{
+    /// <summary>
+    /// Utilities for strings.
+    /// </summary>
+    public static class StringUtilities
+    {
+        /// <summary>
+        /// Pads a string with spaces.
+        /// </summary>
+        /// <param name="source">The source string.</param>
+        /// <param name="newLength">The new (minimum) length of the string.</param>
+        /// <returns></returns>
+        public static string PadLeft(string source, int newLength)
+        {
+            return "meh";
+        }
+    }
+}
